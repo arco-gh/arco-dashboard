7ea741a14279ba14b3c764f52172bc42efb6be15
arco-gh <arco.gml@gmail.com>
2026-03-27 10:57:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1328,8 +1328,6 @@ else:
     <div class="section-title">Validación de hipótesis</div>
     """, unsafe_allow_html=True)
 
-    h_col1 = st.columns(1)
-    with h_col1:
         mape_prophet = modelos["mape_af_prophet"]   # 5.05% del Módulo 2B
         mape_af_base = modelos["mape_af"]            # Modelo A baseline
         prec_prophet = 100 - mape_prophet
